4fb399711d407916bda6080dbcf28b49f278fe7e
sha9n <21stason@gmail.com>
2023-03-15 17:28:14 +0300

    Добавил воозможность писать числа и знаки

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let a = '';
-let b = '';
+let a = ''; 
+let b = ''; 
 let sign = '';
-let finish = false;
+let finish  = false;
 
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 const action = ['-', '+', 'X', '/'];
-const out = document.querySelector('.calc-screen p');
+
+const out = document.querySelector('.screen p');
 
 function clearAll () {
-    a = '';
+    a = ''; 
     b = '';
     sign = '';
     finish = false;
@@ -18,15 +19,36 @@ function clearAll () {
 document.querySelector('.ac').onclick = clearAll;
 
 document.querySelector('.buttons').onclick = (event) => {
-    if(event.target.classList.constains('knop')) return;
-    if(event.target.classList.constains('ac')) return;
+    if(!event.target.classList.contains('knop')) return;
+    if(event.target.classList.contains('ac')) return;
 
     out.textContent = '';
-    const key = event.target.textContent; 
+    const key = event.target.textContent;
 
     if (digit.includes(key)) {
-        a += key;
-        console.log(a , b, sign)
-        out.textContent = a;
+        if (b ==='' && sign === '') {
+            a += key;
+            
+            out.textContent = a;
+        }
+        else if (a!=='' && b!=='' && finish) {
+            b = key;
+            finish = false;
+            out.textContent = b;
+        }
+        else {
+            b += key;
+            out.textContent = b;
+        }
+        console.table(a, b , sign);
+        return;
     }
+
+     if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.table(a, b , sign);
+        return;
+    }
+    
 }
