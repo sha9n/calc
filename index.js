3222dcff9db3c602dcfe716f8aa1cef97e737777
Stas Yaprintsev <21stason@gmail.com>
2023-03-17 18:38:19 +0300

    Немного изменил код

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let finish  = false;
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 const action = ['-', '+', 'X', '÷'];
 
-const out = document.querySelector('.screen p');
+const out = document.querySelector('.screen').querySelector('p');
 
 function clearAll () {
     a = ''; 
@@ -14,6 +14,57 @@ function clearAll () {
     sign = '';
     finish = false;
     out.textContent = 0;
+}
+
+function handleDigitClick(key) {
+    if (b ==='' && sign === '') {
+        a += key;
+        out.textContent = a;
+    }
+    else if (a!=='' && b!=='' && finish) {
+        b = key;
+        finish = false;
+        out.textContent = b;
+    }
+    else {
+        b += key;
+        out.textContent = b;
+    }
+    console.table(a, b , sign);
+}
+
+function handleActionClick(key) {
+    sign = key;
+    out.textContent = sign;
+    console.table(a, b , sign);
+}
+
+function handleEqualsClick() {
+    if (b ==='') b = a;
+    switch (sign) {
+        case "+":
+            a = (+a) + (+b);
+            break;
+        case "-":
+            a = a - b;
+            break;
+        case "X":
+            a = a * b;
+            break;
+        case "÷":
+            if (b === '0') {
+                out.textContent = 'Разделить на ноль нельзя';
+                a = '';
+                b = '';
+                sign = '';
+                return;
+            }
+            a = a / b;
+            break;
+    }
+    finish = true;
+    out.textContent = a;
+    console.table(a, b , sign);
 }
 
 document.querySelector('.ac').onclick = clearAll;
@@ -26,57 +77,17 @@ document.querySelector('.buttons').onclick = (event) => {
     const key = event.target.textContent;
 
     if (digit.includes(key)) {
-        if (b ==='' && sign === '') {
-            a += key;
-            
-            out.textContent = a;
-        }
-        else if (a!=='' && b!=='' && finish) {
-            b = key;
-            finish = false;
-            out.textContent = b;
-        }
-        else {
-            b += key;
-            out.textContent = b;
-        }
-        console.table(a, b , sign);
+        handleDigitClick(key);
         return;
     }
 
      if (action.includes(key)) {
-        sign = key;
-        out.textContent = sign;
-        console.table(a, b , sign);
+        handleActionClick(key);
         return;
     }
 
     if (key === '=') {
-        if (b ==='') b = a;
-        switch (sign) {
-            case "+":
-                a = (+a) + (+b);
-                break;
-            case "-":
-                a = a - b;
-                break;
-            case "X":
-                a = a * b;
-                break;
-            case "÷":
-                if (b === '0') {
-                    out.textContent = 'Разделить на ноль нельзя';
-                    a = '';
-                    b = '';
-                    sign = '';
-                    return;
-                }
-                a = a / b;
-                break;
-        }
-        finish = true;
-        out.textContent = a;
-        console.table(a, b , sign);
+        handleEqualsClick();
     }
 
 }
