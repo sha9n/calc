538c554fcc75ee9eb2056406136ee34fde577975
Stas Yaprintsev <21stason@gmail.com>
2023-03-21 18:42:45 +0300

    Data-action

--- index.js
+++ index.js
@@ -1,93 +1,97 @@
-let a = ''; 
-let b = ''; 
-let sign = '';
-let finish  = false;
+let a = "";
+let b = "";
+let sign = "";
+let finish = false;
 
-const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const action = ['-', '+', 'X', '÷'];
+const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const action = ["-", "+", "X", "÷",];
 
-const out = document.querySelector('.screen').querySelector('p');
+const out = document.querySelector(".out");
 
-function clearAll () {
-    a = ''; 
-    b = '';
-    sign = '';
-    finish = false;
-    out.textContent = 0;
+function clearAll() {
+  a = "";
+  b = "";
+  sign = "";
+  finish = false;
+  out.textContent = 0;
 }
 
 function handleDigitClick(key) {
-    if (b ==='' && sign === '') {
-        a += key;
-        out.textContent = a;
-    }
-    else if (a!=='' && b!=='' && finish) {
-        b = key;
-        finish = false;
-        out.textContent = b;
-    }
-    else {
-        b += key;
-        out.textContent = b;
-    }
-    console.table(a, b , sign);
+  if (b === "" && sign === "") {
+    a += key;
+    out.textContent = a;
+  } else if (a !== "" && b !== "" && finish) {
+    b = key;
+    finish = false;
+    out.textContent = b;
+  } else {
+    b += key;
+    out.textContent = b;
+  }
+  console.table(a, b, sign);
 }
 
 function handleActionClick(key) {
-    sign = key;
-    out.textContent = sign;
-    console.table(a, b , sign);
+  sign = key;
+  out.textContent = sign;
+  console.table(a, b, sign);
 }
 
 function handleEqualsClick() {
-    if (b ==='') b = a;
-    switch (sign) {
-        case "+":
-            a = (+a) + (+b);
-            break;
-        case "-":
-            a = a - b;
-            break;
-        case "X":
-            a = a * b;
-            break;
-        case "÷":
-            if (b === '0') {
-                out.textContent = 'Разделить на ноль нельзя';
-                a = '';
-                b = '';
-                sign = '';
-                return;
-            }
-            a = a / b;
-            break;
-    }
-    finish = true;
-    out.textContent = a;
-    console.table(a, b , sign);
+  if (b === "") b = a;
+  switch (sign) {
+    case "+":
+      a = +a + +b;
+      break;
+    case "-":
+      a = a - b;
+      break;
+    case "X":
+      a = a * b;
+      break;
+    case "÷":
+      if (b === "0") {
+        out.textContent = "Ошибка";
+        a = "";
+        b = "";
+        sign = "";
+        return;
+      }
+      a = a / b;
+      break;
+  }
+  finish = true;
+  out.textContent = a;
+  console.table(a, b, sign);
 }
 
-document.querySelector('.ac').onclick = clearAll;
+document.querySelector(".ac").onclick = clearAll;
 
-document.querySelector('.buttons').onclick = (event) => {
-    if(!event.target.classList.contains('knop')) return;
-    if(event.target.classList.contains('ac')) return;
+document.querySelector(".buttons").onclick = (event) => {
+  if (!event.target.classList.contains("knop")) return;
+  if (event.target.classList.contains("ac")) return;
 
-    out.textContent = '';
-    const key = event.target.textContent;
+  out.textContent = "";
+  const key = event.target.textContent;
 
-    if (digit.includes(key)) {
-        handleDigitClick(key);
-        return;
-    }
+  if (digit.includes(key)) {
+    handleDigitClick(key);
+    return;
+  }
 
-     if (action.includes(key)) {
-        handleActionClick(key);
-        return;
-    }
+  if (action.includes(key)) {
+    handleActionClick(key);
+    return;
+  }
 
-    if (key === '=') {
-        handleEqualsClick();
-    }
+  if (key === "=") {
+    handleEqualsClick();
+  }
+};
 
-}
+[...document.querySelectorAll('[data-action]')].forEach((actionNode) => {
+	actionNode.addEventListener('click', () => {
+  	const operation = actionNode.dataset.action
+    console.log('operation', operation)
+  })
+})
